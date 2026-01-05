b25764e4b0bf6dbe827a3b5947ed7be119e413fe
soni30141241 <soni30141241@gmail.com>
2026-01-05 22:18:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,8 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
 YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key
+
+UPSTREAM_REPO = getenv(
 "UPSTREAM_REPO",
     "https://github.com/soni30141241/Babanewrepo",
 )
@@ -119,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
